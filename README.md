d77cae024d32bfecf4b4c756211f0ec4737d0166
sbssstc145 <114373390+sbssstc145@users.noreply.github.com>
2022-10-12 10:43:21 +0530

    Add files via upload

---
+++ README.md
@@ -0,0 +1,14 @@
+## forest fire predictor application using flask 
+pre-requirements:
+python 3.10
+
+install following packages using pip command:
+syntax: pip install package_name
+
+flask==2.0.2
+sklearn==0.0
+pandas==1.3.5
+numpy== 1.22.0
+matplotlib== 3.5.1
+gunicorn==20.1.0
+
---
+++ app.py
@@ -0,0 +1,39 @@
+from flask import*
+from flask import Flask, request, url_for, redirect, render_template
+import pickle
+import numpy as np
+
+app = Flask(__name__)
+
+model = pickle.load(open('model.pkl', 'rb'))
+
+
+@app.route("/data", methods=["POST", "GET"])
+def predict():
+    if request.method == "POST":
+        oxygen = request.form.get("oxygen")
+        temperature = request.form.get("temperature")
+        humidity = request.form.get("humidity")
+        result = [oxygen, temperature, humidity]
+
+        inp = np.array([int(i) for i in result])
+        prediction = model.predict_proba([inp])
+        output = '{0:.{1}f}'.format(prediction[0][1], 2)
+        # if output > str(0.5):
+        #     return render_template('firepred.html', pred='Your Forest is in Danger.\nProbability of fire occuring is {}'.format(output))
+        # else:
+        #     return render_template('firepred.html', pred='Your Forest is safe.\n Probability of fire occuring is {}'.format(output))
+        # return "get"+" "+output
+        res = result
+        res.append(output)
+
+        return render_template("success.html", result=res)
+
+        # return result[0]
+    return render_template("firepred.html")
+# @app.route('/',)
+# def predict():
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
